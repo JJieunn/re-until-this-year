9176426612cc4f4f88efa12251ec81739adeb9a4
JJieunn <lancia1228@gamil.com>
2023-02-06 01:18:29 +0900

    Add: 유저 정보(이메일, 닉네임, 수신 동의, 운, 목표) 등록
    유저 정보(이메일, 닉네임, 수신 동의, 운, 목표) 등록
      - 유저가 입력한 정보 등록
      - email, nickname, fortune_id, goals 필수 값이 전달되지 않는 경우 예외 처리
      - 등록할 email, nickname을 정규표현식으로 검증
      - 문자열로 전달된 fortune_id를 getFortuneIdByName 함수를 통해 매칭되는 idx 값으로 변환
      - checkEmail을 통해 기존에 이용했던 유저라면 updateGoals로 분기 처리
      - 이미 Goals에 있던 목표를 delete 후 새로운 목표 insert
      - Goals 값들은 data 나열로 한번에 insert 하기 위해 values 쿼리문을 작성하는 반복문 사용
      - 유저 정보 등록으로 Goals, Users 각각의 테이블에 변동사항 있을 때, 원자성을 위해 transaction 사용

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
-import { CheckDataDTO } from "../dto/userDTO";
+import { CheckDataDTO, CreateDataDTO } from "../dto/userDTO";
 import { DuplicateException, EmptyException } from "../middlewares/exceptions";
+import { regExpCheck } from "../middlewares/regexpCheck";
 import userService from "../services/users";
 
 
@@ -13,8 +14,21 @@ const checkEmail = async (req: Request, res: Response) => {
   else { return res.status(200).json({ message: "Available" }); }
 }
 
+const createUserInfo = async (req: Request, res: Response) => {
+  const data: CreateDataDTO = req.body;
+  if(!(data.email && data.nickname && data.email && data.fortune_id && data.goals )) {
+    throw new EmptyException("Input_Error"); }
+  
+  let regExpResult = regExpCheck(data);
+
+  if(regExpResult) { 
+    await userService.createUserInfo(data);
+    return res.status(201).json({ message : "User_Created" }) }
+}
+
 
 
 export default {
-  checkEmail
+  checkEmail,
+  createUserInfo
 }
